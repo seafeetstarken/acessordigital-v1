67ae1bf2a592de3b444d19309acadd18f5aa6253
ferramentastecnologia <ferramentas.starken@gmail.com>
2026-06-30 02:39:12 -0300

    fix: update server credentials and client project config to acessordigital-v1

--- app/firebase-config.js
+++ app/firebase-config.js
@@ -1,7 +1,7 @@
 export const FIREBASE_CONFIG = {
   apiKey: "AIzaSyB-6dxZaPtFvX0MIbARcaeXd2xXjQHNDcU",
   authDomain: "acessordigital-v1.firebaseapp.com",
-  projectId: "seafeet-starken-core",
+  projectId: "acessordigital-v1",
   storageBucket: "acessordigital-v1.firebasestorage.app",
   messagingSenderId: "646046956718",
   appId: "1:646046956718:web:473bb10954d7007989413f",
